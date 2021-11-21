28ef6e8469ccd45d3bf1f879e0457417816d4ef6
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-11-21 17:19:30 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var app = express();
+
+app.post("/auth", (req, res) => {
+  console.log(req.headers);
+  console.log(req.body);
+  res.send({
+    success: true,
+  });
+});
+
+const server = app.listen(4000, onListening);
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+async function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+  console.log(`Listening on ${bind}`);
+}
+
+server.on("error", onError);
+
+module.exports = server;
